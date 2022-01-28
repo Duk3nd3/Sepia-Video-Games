6b9c9839e7c20ca491e60c1ac67a4599d6784745
Duk3nd3 <hjmv1982@outlook.com.ar>
2022-01-27 21:58:46 -0300

    JSX y transpiling & Components l

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,19 @@
 import './NavBar.css';
+import ShoppingCartCheckoutSharpIcon from '@mui/icons-material/ShoppingCartCheckoutSharp';
 
 export const NavBar = () => {
 
     return (
 
         <header className="header">
-            <h1>LOGO</h1>
+            <h1>S3PiA</h1>
             <nav className="header-nav">
-                <p className="header-link">Enlace 1</p>
-                <p className="header-link">Enlace 2</p>
-                <p className="header-link">Enlace 3</p>
+                <p className="header-link">Consolas</p>
+                <p className="header-link">Cartuchos</p>
+                <p className="header-link">Arcades</p>
+                <p className="header-link">Remates</p>
+                <p className="header-link">Coleccionables</p>
+                <ShoppingCartCheckoutSharpIcon className="shopping-cart" fontSize="large"/>
             </nav>
         </header>
 
