284c7c49d4a7636971846117b6a9d8a585570be1
Juan Manuel Vargas <duk3nd3@gmail.com>
2022-03-03 17:53:40 -0300

    Agregando Checkout al proyecto parte 3 + optimizacion en componentes

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,54 +1,15 @@
-import {
-	collection,
-	addDoc,
-	Timestamp,
-	updateDoc,
-	doc,
-	getDoc,
-} from 'firebase/firestore';
 import { useContext, useState } from 'react';
-import { SiRiotgames } from 'react-icons/si';
-import { MdRemoveShoppingCart } from 'react-icons/md';
-import { Link } from 'react-router-dom';
 import { CartContext } from '../Context/CartContext';
 // import Swal from 'sweetalert2';
-import { ddbb } from '../../Firebase/Config';
+import { ThanksforPurchasing } from './ThanksForPurchasing';
+import { Empty } from './Empty';
+import { createOrder } from '../../Firebase/createOrder';
+import { validateFormCheckout } from './ValidateFormCheckout';
 
 export const Checkout = () => {
 	const { cart, totalInCartPrice, cleanCart } = useContext(CartContext);
 
 	const [orderId, setOrderId] = useState(null);
-
-	const createOrder = () => {
-		//Creamos el objeto order
-		const order = {
-			buyer: values,
-			items: cart,
-			total: totalInCartPrice(),
-			purchaseDate: Timestamp.fromDate(new Date()),
-		};
-
-		//Armamos la referencia a la coleccion 'orders', sino existe, la crea
-		const orderReference = collection(ddbb, 'orders');
-
-		//Agregamos el documento a la coleccion en Firebase
-		addDoc(orderReference, order).then((document) => {
-			//Control sobre el stock de los productos
-			cart.forEach((product) => {
-				const documentReference = doc(ddbb, 'stock', product.id);
-				//CON ESTE GET OBTENEMOS LA INFO DEL DOCUMENTO
-				getDoc(documentReference).then((infoDataBaseDocument) => {
-					//UNA VEZ OBTENIDA LA INFO, ACTUALIZAMOS EL STOCK
-					updateDoc(documentReference, {
-						stock: infoDataBaseDocument.data().stock - product.cantidad,
-					});
-				});
-			});
-
-			setOrderId(document.id);
-			cleanCart();
-		});
-	};
 
 	const [values, setValues] = useState({
 		name: '',
@@ -65,60 +26,17 @@ export const Checkout = () => {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-
-		//VALIDAR FORMULARIO
-
-		createOrder();
+		validateFormCheckout(values) && createOrder(values, cart, totalInCartPrice, cleanCart, setOrderId);
 	};
 
 	//SI TENEMOS UNA ORDEN DE COMPRA, EJECUTAMOS LO SIGUIENTE
 	if (orderId) {
-		return (
-			<>
-				<div className='container my-4'>
-					<h2>Gracias por elegirnos</h2>
-					<hr />
-					<div>Tu numero de orden es: {orderId}</div>
-				</div>
-				<Link to='/'>
-					<SiRiotgames
-						style={{
-							color: 'green',
-							fontSize: '40px',
-							marginRight: '50px',
-							marginBottom: '25px',
-							marginTop: '25px',
-							marginLeft: '125px',
-						}}
-					/>
-					{'Quiero seguir comprando!'}
-				</Link>
-			</>
-		);
+		return <ThanksforPurchasing order={orderId}/>
 	}
 
 	//CHECKOUT NO DEBE ESTAR VACIO
 	if (cart.length === 0) {
-		return (
-			<div className='container my-4'>
-				<h2>Tu Compra</h2>
-				<hr />
-				<div>Aun no has comprado nada</div>
-				<Link to='/'>
-					<MdRemoveShoppingCart
-						style={{
-							color: 'green',
-							fontSize: '40px',
-							marginRight: '50px',
-							marginBottom: '25px',
-							marginTop: '25px',
-							marginLeft: '5px',
-						}}
-					/>
-					{'Click aca para ver nuestros clasicos'}
-				</Link>
-			</div>
-		);
+		return <Empty/>
 	}
 
 	return (
